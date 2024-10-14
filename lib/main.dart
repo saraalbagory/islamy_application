05ca995c15a494ff8d3c008d609e12d5fb104d51
saraalbagory <saraanabil123@gmail.com>
2024-10-14 12:43:48 -0700

    did shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:islamy_application/common/app_theme.dart';
-
+//import 'package:islamy_application/user_preference.dart';
+import 'package:provider/provider.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 import 'package:islamy_application/providers/theme_provider.dart';
 import 'package:islamy_application/screens/hadeth_content_screen.dart';
 import 'package:islamy_application/screens/home_screen.dart';
 import 'package:islamy_application/screens/quraan_verses_screen.dart';
-import 'package:provider/provider.dart';
+//import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -23,10 +25,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+   
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+   
+    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
